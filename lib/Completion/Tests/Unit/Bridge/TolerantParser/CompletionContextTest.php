40dca4ade5325f3ed77cea1a7c2469b9d01bc169
przepompownia <przepompownia@users.noreply.github.com>
2026-01-14 10:34:02 +0100

    KeywordCompletor: complete statement keywords

--- lib/Completion/Tests/Unit/Bridge/TolerantParser/CompletionContextTest.php
+++ lib/Completion/Tests/Unit/Bridge/TolerantParser/CompletionContextTest.php
@@ -105,6 +105,222 @@ class CompletionContextTest extends TestCase
         ];
     }
 
+    #[DataProvider('provideStatement')]
+    public function testStatement(string $source, bool $expected): void
+    {
+        [$source, $offset] = ExtractOffset::fromSource($source);
+        $node = (new TolerantAstProvider())->get(TextDocumentBuilder::fromString($source))->getDescendantNodeAtPosition((int)$offset);
+        self::assertEquals($expected, CompletionContext::statement($node, ByteOffset::fromInt($offset)));
+    }
+
+    /**
+     * @return Generator<string,array<int,mixed>>
+     */
+    public static function provideStatement(): Generator
+    {
+        yield 'root' => [
+            '<?php <>',
+            true,
+        ];
+        yield 'in namespace' => [
+            '<?php namespace X; <>',
+            false, // disabled while no idea how to distinguish this case from that if the cursor is inside comment
+        ];
+        yield 'in comment' => [
+            '<?php 
+            namespace X; 
+            /**
+            <>
+            */',
+            false,
+        ];
+        yield 'statement property' => [
+            '<?php class Foo { pri<> }',
+            false,
+        ];
+        yield 'statement visibility 1' => [
+            '<?php class Foo { <> }',
+            false,
+        ];
+        yield 'statement visibility 2' => [
+            '<?php class Foo { private <> }',
+            false,
+        ];
+        yield 'visibility 3' => [
+            '<?php class Foo { private Foob<> }',
+            false,
+        ];
+        yield 'statement method body' => [
+            '<?php class Foo { private function foo() { <> } }',
+            true,
+        ];
+        yield 'nested statement method body' => [
+            '<?php class Foo { private function foo() { if (true) { $x = 1; re<> } } }',
+            true,
+        ];
+        yield 'case statement' => [
+            '<?php class Foo { private function foo() { switch (true) { case 1: <> } } }',
+            true,
+        ];
+        yield 'case statement 2' => [
+            '<?php class Foo { private function foo() { switch (true) { case 1: ret<> } } }',
+            true,
+        ];
+        yield 'in while body' => [
+            '<?php class Foo { private function foo() { while () {<>} } }',
+            true,
+        ];
+        yield 'in while body descendant' => [
+            '<?php class Foo { private function foo() { while () { re<>} } }',
+            true,
+        ];
+
+        yield 'string literal' => [
+            '<?php class Foo { private function foo() { "<>" }',
+            false,
+        ];
+        yield 'string literal 2' => [
+            '<?php class Foo { private function foo() { "rrr<>" }',
+            false,
+        ];
+        yield 'switch condition' => [
+            '<?php class Foo { private function foo() { switch (<>) {} } }',
+            false,
+        ];
+        yield 'switch condition 2' => [
+            '<?php class Foo { private function foo() { switch (re<>) {} } }',
+            false,
+        ];
+        yield 'foreach condition' => [
+            '<?php class Foo { private function foo() { foreach (<>) {} } }',
+            false,
+        ];
+        yield 'foreach condition 2' => [
+            '<?php class Foo { private function foo() { foreach (re<>) {} } }',
+            false,
+        ];
+        yield 'foreach condition key' => [
+            '<?php class Foo { private function foo() { foreach ($x as re<>) {} } }',
+            false,
+        ];
+        yield 'for condition 1' => [
+            '<?php class Foo { private function foo() { for (<>) {} } }',
+            false,
+        ];
+        yield 'for condition 2' => [
+            '<?php class Foo { private function foo() { for (true, <>) {} } }',
+            false,
+        ];
+        yield 'for condition 1 descendant 1' => [
+            '<?php class Foo { private function foo() { for (re<>) {} } }',
+            false,
+        ];
+        yield 'for condition 2 descendant 1' => [
+            '<?php class Foo { private function foo() { for (true, re<>) {} } }',
+            false,
+        ];
+        yield 'for condition 1 descendant 2' => [
+            '<?php class Foo { private function foo() { for ($x<>) {} } }',
+            false,
+        ];
+        yield 'catch selector' => [
+            '<?php class Foo { private function foo() { try {} catch (<>) {} } }',
+            false,
+        ];
+        yield 'catch selector descendant' => [
+            '<?php class Foo { private function foo() { try {} catch (re<>) {} } }',
+            false,
+        ];
+        yield 'in do condition' => [
+            '<?php class Foo { private function foo() { do {} while (<>) } }',
+            false,
+        ];
+        yield 'in do condition descendant' => [
+            '<?php class Foo { private function foo() { do {} while (re<>) } }',
+            false,
+        ];
+        yield 'in if condition' => [
+            '<?php class Foo { private function foo() { if (<>) {} } }',
+            false,
+        ];
+        yield 'in if condition descendant' => [
+            '<?php class Foo { private function foo() { if (tr<>) {} } }',
+            false,
+        ];
+        yield 'in while condition' => [
+            '<?php class Foo { private function foo() { while (<>) {} } }',
+            false,
+        ];
+        yield 'in while condition descendant' => [
+            '<?php class Foo { private function foo() { while (tr<>) {} } }',
+            false,
+        ];
+        yield 'statement visibility 4' => [
+            '<?php class Foo { private Foobles <> }',
+            false,
+        ];
+        yield 'statement visibility 5' => [
+            '<?php class Foo { private Foobles $<> }',
+            false,
+        ];
+        yield 'statement after class' => [
+            '<?php class Foo { private Foobles $foo; } $foo-><>',
+            false,
+        ];
+        yield 'statement const value' => [
+            '<?php class Foo { public const X = sel<> }',
+            false,
+        ];
+        yield 'statement const value 2' => [
+            '<?php class Foo { public const X = [sel<> }',
+            false,
+        ];
+        yield 'statement attribute' => [
+            '<?php class Foo { public function baz(){} #[Foo\<>]public function bar(){}}',
+            false,
+        ];
+        yield 'after member access 1' => [
+            '<?php class Foo { private function foo() { $this-><> } }',
+            false,
+        ];
+        yield 'after member access 2' => [
+            '<?php class Foo { private function foo() { return $this-><> } }',
+            false,
+        ];
+        yield 'after static access 1' => [
+            '<?php class Foo { private function foo() { return self::<> } }',
+            false,
+        ];
+        yield 'echo 1' => [
+            '<?php class Foo { private function foo() { $t = 1; echo <>; $t = 2; } }',
+            false,
+        ];
+        yield 'echo 2' => [
+            '<?php class Foo { private function foo() { echo <>; $t = 2; } }',
+            false,
+        ];
+        yield 'echo 3' => [
+            '<?php class Foo { private function foo() { echo <>; } }',
+            false,
+        ];
+        yield 'echo 4' => [
+            '<?php class Foo { private function foo() { $t = 1; echo <> } }',
+            false,
+        ];
+        yield 'echo 5' => [
+            '<?php class Foo { private function foo() { echo <> $t = 1;} }',
+            false,
+        ];
+        yield 'at the end of full name' => [
+            '<?php class F { public function foo() { while<> ',
+            true,
+        ];
+        yield 'parent is stmt, inside parens' => [
+            '<?php class F { public function foo() { while (<> ',
+            false,
+        ];
+    }
+
     #[DataProvider('provideClassMemberBody')]
     public function testClassMemberBody(string $source, bool $expected): void
     {
